56bb6335c86dac513e919bb91e070da6c2a7d278
Ivan <ivanprats@hey.com>
2022-01-29 12:45:22 +0100

    Installing and using highlight.js for code styles

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,10 @@ import '../css/app.css'
 import Alpine from 'alpinejs'
 window.Alpine = Alpine
 Alpine.start()
+
+import hljs from 'highlight.js/lib/common'
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelectorAll('pre code').forEach((el) => {
+    hljs.highlightElement(el)
+  })
+})
